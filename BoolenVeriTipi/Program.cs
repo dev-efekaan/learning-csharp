347b248326dbc7d7de65a9e3805c2e8f04ce36f1
Efe Kaan Kavalcı <efekaankavalci58@gmail.com>
2025-10-28 22:46:29 +0300

    Add files via upload

---
+++ BoolenVeriTipi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoolenVeriTipi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool kontrol1= true;
+            bool kontrol2= false;
+
+            bool sonuc = 9 > 10;
+            Console.WriteLine(sonuc);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ CharVeriTipi/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharVeriTipi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Karakteri tutmak için cahr veri tipi kullanılır
+            // Tek bir karakter girilir ve tek tırnakla kapatılır
+
+            char karakter1 = 'E'; //örnektir
+            char karakter2 = 'F';
+            char karakter3 = 'K';
+            char karakter4 = 'A';
+            char karakter5 = 'N';
+            
+
+            Console.Write(karakter1);
+            Console.Write(karakter2);
+            Console.Write(karakter1);
+            Console.Write(karakter3);
+            Console.Write(karakter4);
+            Console.Write(karakter4);
+            Console.Write(karakter5);
+            Console.ReadLine();
+            
+
+        }
+    }
+}
